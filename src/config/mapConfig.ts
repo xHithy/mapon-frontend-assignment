c1870f090ad4bdf80bd3d187a55e3a9fbcd42416
Hithy <hithyisgod@gmail.com>
2023-12-11 18:37:09 +0200

    Create mapConfig.ts

---
+++ src/config/mapConfig.ts
@@ -0,0 +1,14 @@
+export const mapOptions = {
+    streetViewControl: false,
+    fullscreenControl: false,
+    scaleControl: false,
+    mapTypeControl: false,
+    panControl: false,
+    zoomControl: false,
+    rotateControl: false
+}
+
+export const mapCenter = {
+    lat: 56.967,
+    lng: 24.105
+}
