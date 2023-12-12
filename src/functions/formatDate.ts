a47a1220ec1953fe1a2e799a9bea8ac4f770202d
Hithy <hithyisgod@gmail.com>
2023-12-12 18:21:49 +0200

    Create formatDate.ts

---
+++ src/functions/formatDate.ts
@@ -0,0 +1,10 @@
+export const formatDate = (input: string): string => {
+    const date = new Date(input);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds} (${day}.${month})`;
+}
