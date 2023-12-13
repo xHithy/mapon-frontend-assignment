01d28290dbe2aa1f49de50be307d74586728a305
Hithy <hithyisgod@gmail.com>
2023-12-13 18:31:41 +0200

    Update RouteMap.tsx
    Made the map only show up if any routes are found
    
    Incase of no routes found, it displays a notification message
    
    Created a loading screen while the map is loading

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -1,25 +1,82 @@
-import React from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { Fragment, useState } from 'react';
+import { GoogleMap, Marker, Polyline, useJsApiLoader } from '@react-google-maps/api';
 import { mapCenter, mapOptions } from "../config/mapConfig";
+import CustomMarker from '../assets/marker.png';
+import RouteSelect from "./RouteSelect";
+import RouteStats from "./RouteStats";
+import { SingleRouteModel } from "../models/RouteModel";
 
-const RouteMap = () => {
-	const { isLoaded } = useJsApiLoader({
+interface RouteMapProps {
+	routes: SingleRouteModel[];
+}
+
+const RouteMap = ({
+	routes
+}: RouteMapProps) => {
+	// Always select the first route in the list when initializing the map component
+	const [selectedRoute, setSelectedRoute] = useState <number> (routes[0].route_id);
+
+	const selectedRouteData = routes.find(route => route.route_id === selectedRoute);
+
+	const { isLoaded, loadError } = useJsApiLoader({
 		id: 'google-map-script',
 		googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY || ''
 	});
 
-	return (
-		isLoaded ? (
+	if (loadError) {
+		return <div>Error loading Google Maps API: {loadError.message}</div>;
+	}
+
+	if(isLoaded) {
+		return (
+		<>
+			<RouteSelect
+				routes={routes}
+				selectedRoute={selectedRoute}
+				setSelectedRoute={setSelectedRoute}
+			/>
 			<GoogleMap
 				mapContainerClassName='w-full h-56'
-				center={ mapCenter }
-				zoom={ 10 }
-				options={ mapOptions }
-			></GoogleMap>
-		) : (
-			<div>Loading...</div>
-		)
-	);
+				center={mapCenter}
+				zoom={10}
+				options={mapOptions}
+			>
+				{selectedRouteData && (
+					<Fragment>
+						<Marker
+							position={{ lat: selectedRouteData.start.lat, lng: selectedRouteData.start.lng }}
+							icon={{
+								url: CustomMarker,
+								scaledSize: new window.google.maps.Size(32, 34)
+							}}
+						/>
+						<Marker
+							position={{ lat: selectedRouteData.end.lat, lng: selectedRouteData.end.lng }}
+							icon={{
+								url: CustomMarker,
+								scaledSize: new window.google.maps.Size(32, 34)
+							}}
+						/>
+						<Polyline
+							path={selectedRouteData.decoded_route.points.map((point: { lat: any; lng: any; }) => ({
+								lat: point.lat,
+								lng: point.lng
+							}))}
+							options={{ strokeColor: '#3BAFFA', strokeWeight: 4 }}
+						/>
+					</Fragment>
+				)}
+			</GoogleMap>
+			{ selectedRouteData &&
+				<RouteStats
+					selectedRouteData={selectedRouteData}
+				/>
+			}
+		</>
+		);
+	}
+
+	return <div className='w-full h-56 bg-gray-200 animate-pulse'></div>;
 };
 
 export default RouteMap;
