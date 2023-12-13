aecdb437f1b4fb45c30873ad5af63b360dc23132
Hithy <hithyisgod@gmail.com>
2023-12-13 18:29:05 +0200

    Create RouteSelect.tsx
    Added a component that allows the user to select one of many routes in a time period, so the map doesn't get too messy with multiple routes overlapping eachother

---
+++ src/components/RouteSelect.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { formatDate } from "../functions/formatDate";
+import { SingleRouteModel } from "../models/RouteModel";
+
+interface RouteSelectProps {
+    routes: SingleRouteModel[];
+    selectedRoute: number,
+    setSelectedRoute: React.Dispatch<React.SetStateAction<number>>
+}
+
+const RouteSelect = ({
+    routes,
+    selectedRoute,
+    setSelectedRoute
+}: RouteSelectProps) => {
+    return (
+        <div className='flex flex-col px-4 sm:justify-between sm:items-center sm:flex-row sm:px-7 mb-5'>
+            <label
+                className='w-1/2'
+                htmlFor='route'
+            >
+                Routes <span className='text-sm text-gray-400'>({routes.length} found)</span>
+            </label>
+            <select
+                name='route'
+                className='outline-gray-200 outline w-full p-2 rounded-sm cursor-pointer'
+                value={selectedRoute}
+                onChange={(e) => setSelectedRoute(parseInt(e.target.value))}
+            >
+                {routes.map((route) => (
+                    <option key={route.route_id} value={route.route_id}>
+                        from {formatDate(route.start.time)} to {formatDate(route.end.time)}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default RouteSelect;
