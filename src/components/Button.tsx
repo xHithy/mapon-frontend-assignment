699043ac415bde0c145d882e65c4c8d53f2e182b
Hithy <hithyisgod@gmail.com>
2023-12-13 18:29:42 +0200

    Update Button.tsx
    Added submit form event to the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Button = () => {
+interface ButtonProps {
+    handleFormSubmit:  React.MouseEventHandler<HTMLButtonElement>
+}
+
+const Button = ({
+    handleFormSubmit
+}: ButtonProps) => {
     return (
         <div className='bg-[#F4F4F4] w-full px-7 py-4 flex justify-end'>
-            <button className='uppercase bg-[#98C907] px-4 py-2 rounded-sm text-sm font-semibold text-white hover:bg-[#80BC00] transition-colors duration-200'>Generate</button>
+            <button onClick={handleFormSubmit} className='uppercase bg-[#98C907] px-4 py-2 rounded-sm text-sm font-semibold text-white hover:bg-[#80BC00] transition-colors duration-200'>Generate</button>
         </div>
     );
 };
