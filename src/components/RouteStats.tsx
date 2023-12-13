eb633d2e9ff34830044d14c2eb2cdc8f512e9765
Hithy <hithyisgod@gmail.com>
2023-12-13 18:28:17 +0200

    Removed unused statistic in the stat component
    Also added responsive stats, so they correspond to the selected route

--- src/components/RouteStats.tsx
+++ src/components/RouteStats.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { calculateTotalDrivingTime } from "../functions/calculateTotalDrivingTime";
+import { SingleRouteModel } from "../models/RouteModel";
 
-const RouteStats = () => {
+interface RouteStatsProps {
+    selectedRouteData: SingleRouteModel;
+}
+
+const RouteStats = ({
+    selectedRouteData
+}: RouteStatsProps) => {
     return (
         <div className='p-7 flex justify-center items-center flex-col px-7 sm:flex-row'>
             <div className='flex flex-col items-center px-9 py-4 sm:py-0'>
-                <span className='text-4xl'>128</span>
+                <span className='text-4xl'>
+                    { (selectedRouteData.distance / 1000).toFixed(1) }
+                </span>
                 <span className='text-gray-500 text-sm'>Km driven</span>
             </div>
-            <div className='flex flex-col items-center px-6 border-y-[1.5px] py-5 w-full sm:w-fit sm:border-x sm:border-y-0 sm:py-0'>
-                <span className='text-4xl'>3h 20m</span>
-                <span className='text-gray-500 text-sm'>Total driving time</span>
-            </div>
-            <div className='flex flex-col items-center px-9 py-4 sm:py-0'>
-                <span className='text-4xl'>1h 5m</span>
+            <div className='flex flex-col items-center px-6 border-t-[1.5px] py-5 w-full sm:w-fit sm:border-l sm:border-y-0 sm:py-0'>
+                <span className='text-4xl'>
+                    { calculateTotalDrivingTime(selectedRouteData.start_time, selectedRouteData.end_time) }
+                </span>
                 <span className='text-gray-500 text-sm'>Driving time</span>
             </div>
         </div>
