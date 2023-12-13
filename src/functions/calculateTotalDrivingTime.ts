69fa08f511179be13702e90c6e8650b91f550a41
Hithy <hithyisgod@gmail.com>
2023-12-13 23:57:15 +0200

    Update calculateTotalDrivingTime.ts

--- src/functions/calculateTotalDrivingTime.ts
+++ src/functions/calculateTotalDrivingTime.ts
@@ -1,14 +1,29 @@
-export const calculateTotalDrivingTime = (start_time: string, end_time: string): string => {
-    const start = new Date(start_time);
-    const end = new Date(end_time);
+import { SingleRouteModel } from "../models/RouteModel";
 
-    const timeDifference = end.getTime() - start.getTime();
+export const calculateTotalDrivingTime = (routes: SingleRouteModel[]) => {
+    let totalDrivingTimeInMinutes = 0;
 
-    // Convert the time difference to hours, minutes, and seconds
-    const totalMilliseconds = Math.abs(timeDifference);
-    const totalSeconds = Math.floor(totalMilliseconds / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    routes.forEach((route) => {
+        const startTime = new Date(route.start_time);
+        const endTime = new Date(route.end_time);
 
-    return `${hours}h ${minutes}m`;
-};
+        const timeDifferenceInMinutes = (endTime.getTime() - startTime.getTime()) / (1000 * 60);
+
+        totalDrivingTimeInMinutes += timeDifferenceInMinutes;
+    });
+
+    const days = Math.floor(totalDrivingTimeInMinutes / (24 * 60));
+    const remainingMinutes = totalDrivingTimeInMinutes % (24 * 60);
+    const hours = Math.floor(remainingMinutes / 60);
+    const minutes = Math.round(remainingMinutes % 60);
+
+    let formattedString = '';
+
+    if (days > 0) {
+        formattedString += `${days}d `;
+    }
+
+    formattedString += `${hours}h ${minutes}m`;
+
+    return formattedString.trim();
+}
